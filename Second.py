4498c840a3dda293f35173a61b9d777ccd6f7a29
Tukamhane <mahajansham01@gmail.com>
2025-01-07 18:55:39 +0530

    update

---
+++ Second.py
@@ -0,0 +1,26 @@
+# Find Maximum number
+# a=int(input("Enter the first:"))
+# b=int(input("Enter the second:"))
+# c=int(input("Enter the Third:"))
+# d=int(input("Enter the Fourth:"))
+
+# if (a>b) and (a>c) and (a>d):
+#     print(a,"is greater")
+# elif (b>c) and (b>d):
+#     print(b,"is greater")
+# elif (c>d):
+#     print(c,"is greater")
+# else:
+#     print(d,"is greater")
+
+#  
+# Find Maximum number
+x=int(input("Enter the first:"))
+y=int(input("Enter the second:"))
+z=int(input("Enter the Third:"))
+
+
+if x==y==z:
+    print("Equal hai")
+else:
+    print("Nahi hai equal")
