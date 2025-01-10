425332ecceb7d5283513dbd496f2e0b93b42abd9
Tukamhane <mahajansham01@gmail.com>
2025-01-10 18:48:58 +0530

    Update 04_For_loop.py

---
+++ 04_For_loop.py
@@ -0,0 +1,32 @@
+# factorial for range function
+# m=int(input("enter "))
+# fact=1
+# for i in range(1,m):
+#     fact=fact*i
+# print(fact)    
+
+# Loop concept
+# for i in range(3):
+#     print('Outer Loop')
+#     for j in range(1,4):
+#         print("Inner loop")
+#     print('Another Loop started')    
+
+# # Table
+# inp=int(input("Enter the number:"))
+# for i in range(1,11):
+#     print(inp*i)
+
+# Table Form
+# num=int(input("Enter the number"))
+# for i in range (1,num+1):
+#      print()
+#      for j in range(1,9):
+#           print(j*i)
+
+# Armstrong number
+my_num=input("Enter the number")
+num=0
+for i in my_num:
+    num=num+(int(i))**len(my_num)
+print(num)    
