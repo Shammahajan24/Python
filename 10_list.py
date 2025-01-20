53a7849cd53e54befc332ef2bec3724c5cea8b91
Tukamhane <mahajansham01@gmail.com>
2025-01-20 18:48:20 +0530

    Create 10_list.py

---
+++ 10_list.py
@@ -0,0 +1,11 @@
+# List Method
+'''Append,extend,insert(1=index,2=value),remove,pop,count,sort'''
+li=[1,2,3]
+li.append(4)
+print(li)
+li.extend([5,6,7])
+print(li)
+
+#  insert the element in the list as per datatype means int to int
+
+#
